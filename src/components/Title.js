6811190b229cdbb577dd0276118f349fac914bed
aknafu12 <kgebre2021@gmail.com>
2023-02-03 01:19:09 -0800

    add componetts with basic mockup

---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Title = () =>{
+    return <h1>Todo App</h1>
+}
+
+export default Title;
---
+++ src/components/ToDoForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+const ToDoForm =()=>{
+    const [InputTodo, setInputTodo] = useState('')
+    
+}
+
